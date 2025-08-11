4a3a0f0e2993ea871d8555168400fafed83de849
khyati141207 <khyati.10179@skvgwalior.org>
2025-08-11 13:20:05 +0530

    Update movies_app.py

--- movies_app.py
+++ movies_app.py
@@ -23,6 +23,7 @@ matches = pd.DataFrame()  # empty by default
 
 
 # when user types something, choosing the right movies
+[
 if user_input1 and user_input2:
     user_input1 = user_input1.lower()
     user_input2 = user_input2.lower()
@@ -46,6 +47,7 @@ if user_input1 and user_input3:
     matches = df[
     (df['genre'].str.lower().str.contains(user_input1) | df['description'].str.lower().str.contains(user_input1)) &
     (df['genre'].str.lower().str.contains(user_input3) | df['description'].str.lower().str.contains(user_input3) | df['language'].str.lower().str.contains(user_input3))
+]
 ]
 
 elif user_input1:
